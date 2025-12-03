639c3439987088d96f1152d30033821c18bf60b2
SanderIsHere <sandersonjunian2@gmail.com>
2025-12-03 23:47:27 +0700

    env key fixed. Now we can use the ai api

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,32 @@
+// import { GoogleGenAI } from "@google/genai";
+const { GoogleGenAI } = require("@google/genai");
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+console.log("API Key:", process.env.GEMINI_API_KEY ? "OK" : "MISSING!");
+
 class Controller {
-  static async readData(req, res) {
+  static async geminiAi(req, res, next) {
+    try {
+      const { prompt } = req.body;
+      const result = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: prompt,
+      });
+      res.status(200).json({
+        result: result.text,
+      });
+    } catch (err) {
+      console.log(err);
+
+      res.status(500).json({ message: err.message });
+    }
+  }
+  static async readData(req, res, next) {
     try {
       res.send("Berhasil");
-    } catch (er) {
+    } catch (err) {
       res.send(err);
     }
   }
--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const express = require("express");
-const multer = require("multer");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-const Controller = require("./controllers/controller");
 require("dotenv").config();
+const express = require("express");
 const app = express();
-const port = express.env.port || 3000;
+const multer = require("multer");
+const cors = require("cors");
+const Controller = require("./controllers/controller");
+const PORT = process.env.PORT || 3000;
 
 const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
 });
-
-// app.use(cors());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", Controller.readData);
+app.post("/ai-gemini", Controller.geminiAi);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on PORT ${PORT}`);
 });
